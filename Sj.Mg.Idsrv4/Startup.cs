0602a14e0afed8b94241992fc512bd3d91f520cd
Test <Test@SMS-LAP-0004>
2017-02-16 12:12:21 +0530

    Added gmail login

--- Sj.Mg.Idsrv4/Startup.cs
+++ Sj.Mg.Idsrv4/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using IdentityServer3.Core.Configuration;
 using IdentityServer3.Core.Services.Default;
+using Microsoft.Owin.Security.Google;
 using Owin;
 using Sj.Mg.Idsrv4.Config;
 using System;
@@ -7,39 +8,57 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using System.Web;
+using //DataBaseConnection;
 
 namespace Sj.Mg.Idsrv4
 {
     public class Startup
     {
+        private readonly object OnAuthenticated;
+
         public void Configuration(IAppBuilder app)
         {
             app.Map("/identity", idsrv =>
             {
-                var idsrvfactory = new IdentityServerServiceFactory()
-                .UseInMemoryClients(Clients.Get())
-                .UseInMemoryScopes(Scopes.Get())
-                .UseInMemoryUsers(Users.Get());
-
-                var corsPolicyService = new DefaultCorsPolicyService()
+                idsrv.UseIdentityServer(new IdentityServerOptions
                 {
-                    AllowAll = true
-                };
-
-                idsrvfactory.CorsPolicyService = new
-                    Registration<IdentityServer3.Core.Services.ICorsPolicyService>(corsPolicyService);
-
-                var opts = new IdentityServerOptions
-                {
-                    Factory = idsrvfactory,
-                    SiteName = "MedGrotto Identity Server",
-                    IssuerUri = AppConstants.Constants.IssuerUri,
-                    PublicOrigin = AppConstants.Constants.StsOrigin,
+                    SiteName = "Embedded IdentityServer",
                     SigningCertificate = LoadCertificate(),
-                    RequireSsl = false,
-                };
 
-                idsrv.UseIdentityServer(opts);
+                    Factory = new IdentityServerServiceFactory()
+                                    .UseInMemoryUsers(Users.Get())
+                                    .UseInMemoryClients(Clients.Get())
+                                    .UseInMemoryScopes(Scopes.Get()),
+
+                    AuthenticationOptions = new IdentityServer3.Core.Configuration.AuthenticationOptions
+                    {
+                        IdentityProviders = ConfigureIdentityProviders
+                    }
+                });
+            });
+        }
+
+        private void ConfigureIdentityProviders(IAppBuilder app, string signInAsType)
+        {
+            app.UseGoogleAuthentication(new Microsoft.Owin.Security.Google.GoogleOAuth2AuthenticationOptions
+            {
+                AuthenticationType = "Google",
+                Caption = "Sign-in with Google",
+                SignInAsAuthenticationType = signInAsType,
+
+                ClientId = "354165830384-tp8cbhf59riubedmel75t1i5uabq5h2d.apps.googleusercontent.com",
+                ClientSecret = "mCWLDjLjItsd-E5bYmaTBvm6",
+                Provider = new GoogleOAuth2AuthenticationProvider()
+                {
+                    OnAuthenticated = async ctx =>
+                    {
+                        //DataBaseFunc db = new DataBaseFunc();
+                        //db.getData(ctx.Email);
+                        //db.insertData(ctx.Name, ctx.Email, ctx.FamilyName, ctx.AccessToken);
+                        //AppConstants.Helper.TokenHelper.DecodeAndWrite(ctx.AccessToken);
+                        //JWT.JsonWebToken.Decode(ctx.AccessToken, Convert.FromBase64String("mCWLDjLjItsd+E5bYmaTBvm6"));
+                    }
+                }
             });
         }
 
@@ -49,5 +68,6 @@ namespace Sj.Mg.Idsrv4
                 string.Format(@"{0}\certificates\idsrv3test.pfx",
                 AppDomain.CurrentDomain.BaseDirectory), "idsrv3test");
         }
+
     }
 }
