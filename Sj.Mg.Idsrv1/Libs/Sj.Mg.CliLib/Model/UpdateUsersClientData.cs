3406bfa92aab016b7a1175f1982fbfd501822c7b
Immanuel Raj <raj@immanuel.co>
2017-05-31 21:30:07 +0530

    chnages

---
+++ Sj.Mg.Idsrv1/Libs/Sj.Mg.CliLib/Model/UpdateUsersClientData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sj.Mg.CliLib.Model
+{
+    public class UpdateUsersClientData
+    {
+        public UserClientsList userClientsList { get; set; }
+        public bool delClient { get; set; }
+        public bool delScope { get; set; }
+        public string delItem { get; set; }
+    }
+
+    public class ReqParamObj
+    {
+        public string scope { get; set; }
+        public string user { get; set; }
+        public string scopeType { get; set; }
+        public string scopeType1 { get; set; }
+    }
+}
